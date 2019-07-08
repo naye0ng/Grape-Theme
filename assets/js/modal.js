0af004e8c2286b7792b29ca07d2becdb46ba1943
naye0ng <nayeong_e@naver.com>
2019-07-09 01:17:01 +0900

    add project detail in modal

---
+++ assets/js/modal.js
@@ -0,0 +1,9 @@
+var modal = '';
+
+function open_modal(id){
+    modal = document.getElementById(id)
+    modal.style.display = "block"
+}
+function close_modal(){
+    modal.style.display = "none"
+}
